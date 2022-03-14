06b1287208b86fbf536d84911eb19054d13d1a96
Eugene Helland <eugenehelland@mail.com>
2022-03-14 08:17:41 -0600

    Added some more detailed feedback to the tests.

--- test/nameInverterTest.js
+++ test/nameInverterTest.js
@@ -7,6 +7,7 @@ describe('nameInverter', function() {
   it('Should return empty string when passed an empty string ', function() {
     const inputName = ' ';
     const expectedOutput = ' ';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
@@ -14,7 +15,9 @@ describe('nameInverter', function() {
 describe('nameInverter', function() {
   it('return a single name when passed a single name', function() {
     const inputName = 'Troy';
-    assert.equal(nameInverter(inputName), 'Troy');
+    const expectedOutput = 'Troy';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
+    assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
 
@@ -22,6 +25,7 @@ describe('nameInverter', function() {
   it('return a single name when passed a single name with extra spaces', function() {
     const inputName = 'Troy  ';
     const expectedOutput = 'Troy';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
@@ -30,6 +34,7 @@ describe('nameInverter', function() {
   it('return a last-name, first-name when passed a first and last-name', function() {
     const inputName = 'Troy Pit';
     const expectedOutput = 'Pit, Troy';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
@@ -38,6 +43,7 @@ describe('nameInverter', function() {
   it('return a last-name, first-name when passed a first and last-name with extra spaces around the names', function() {
     const inputName = ' Troy Pit';
     const expectedOutput = 'Pit, Troy';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
@@ -46,6 +52,7 @@ describe('nameInverter', function() {
   it('return an empty string when passed a single honorific', function() {
     const inputName = 'Miss.';
     const expectedOutput = ' ';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
@@ -54,6 +61,7 @@ describe('nameInverter', function() {
   it('return honorific first-name when passed honorific first-name', function() {
     const inputName = 'Miss. Troy';
     const expectedOutput = 'Miss. Troy';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
@@ -62,6 +70,7 @@ describe('nameInverter', function() {
   it('return a honorific last-name, first-name when passed honorific first-name last-name', function() {
     const inputName = 'Dr. Troy Pit';
     const expectedOutput = 'Dr. Pit, Troy';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
@@ -70,6 +79,7 @@ describe('nameInverter', function() {
   it('return a honorific last-name, first-name when passed honorific first-name last-name with extra spaces around the words', function() {
     const inputName = '  Dr. Troy Pit  ';
     const expectedOutput = 'Dr. Pit, Troy';
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
@@ -78,6 +88,7 @@ describe('nameInverter', function() {
   it('throw an error when name is undefined', function() {
     const inputName = undefined;
     const expectedOutput = "throw 'Error'";
+    console.log(`Input: ${inputName} --> Output: ${nameInverter(inputName)}`);
     assert.equal(nameInverter(inputName), expectedOutput);
   });
 });
